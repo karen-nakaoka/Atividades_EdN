40db9ade9b3bebbe562fc3cf952a5f694290ecba
Karen Nakaoka <karenkn.28@gmail.com>
2025-06-24 11:54:53 -0300

    Primeiro commit

---
+++ Atividade 1/calculadora_preco_total.py
@@ -0,0 +1,7 @@
+nome_prod = "Cadeira Infantil"
+preco_prod = 12.40
+quant_prod = 3
+
+preco_total = preco_prod * quant_prod
+
+print(f"O preço total das {quant_prod} unidades de {nome_prod} foi de R$ {preco_total:.2f}.")
